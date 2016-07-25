bd87df2c4f9579f71b6d64be5cd1ae61d344d14f
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2016-07-25 14:07:38 +0300

    temp upd

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,13 +4,7 @@ import React from 'react';
 export default function Wrapper(props) {
 	return (
 		<div className="entry">
-			<div className="container">
-				<div className="row">
-					<div className="col-xs-12 col-md-8 col-md-offset-2">
-						{props.children}
-					</div>
-				</div>
-			</div>
+			{props.children}
 		</div>
 
 	)
